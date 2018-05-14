79a424bb55996ee657d786dd5740a076584c7092
MichaC <kletterer@web.de>
2018-05-14 22:52:31 +0200

    fixing and testing the new version

--- src/Runner/Program.cs
+++ src/Runner/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -10,7 +9,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Rest;
 using Polly;
-using Shared;
 using Swagger;
 
 namespace Runner
@@ -21,38 +19,34 @@ namespace Runner
         {
             var services = new ServiceCollection();
 
-            services.AddHttpClient<IPetStoreClient, PetStoreClient>(h =>
-            {
-                // hard timeout doesn't cause retries?!
-                h.Timeout = TimeSpan.FromSeconds(30);
-            })
-            .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(4, (t) => TimeSpan.FromSeconds(t)));
+            services.AddHttpClient<IPetStoreClient, PetStoreClient>()
+                .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(4, (t) => TimeSpan.FromSeconds(t)));
 
-            services.Configure<ClientSettings>(nameof(IPetStoreClient), settings =>
-            {
-                // probably resolve base uri from service discovery...
-                // doesn't work well inside the configure factory because we don't have access to IServiceProvider...
-                settings.BaseUri = new Uri("http://petstore.swagger.io/v2");
+            //services.Configure<ClientSettings>(nameof(IPetStoreClient), settings =>
+            //{
+            //    // probably resolve base uri from service discovery...
+            //    // doesn't work well inside the configure factory because we don't have access to IServiceProvider...
+            //    settings.BaseUri = new Uri("http://petstore.swagger.io/v2");
 
-                // probably use ITokenProvider to get to user's token or retrieve a client credentials token...
-                // same here, how do we resolve a ITokenProvider for this client?
-                settings.Credentials = new TokenCredentials("superToken");
-            });
+            //    // probably use ITokenProvider to get to user's token or retrieve a client credentials token...
+            //    // same here, how do we resolve a ITokenProvider for this client?
+            //    settings.Credentials = new TokenCredentials("superToken");
+            //});
 
-            var provider = services.BuildServiceProvider();
+            //var provider = services.BuildServiceProvider();
 
-            var client = provider.GetRequiredService<IPetStoreClient>();
+            //var client = provider.GetRequiredService<IPetStoreClient>();
 
-            var pets = client.FindPetsByStatus(new List<string>() { "available", "pending" });
+            //var pets = client.FindPetsByStatus(new List<string>() { "available", "pending" });
 
-            Console.WriteLine($"There are {pets.Count} pets.");
+            //Console.WriteLine($"There are {pets.Count} pets.");
 
             /*** more real implementation: ***/
 
             // no implementation here, just dummy here...
-            services.AddSingleton<IServiceDiscovery>();
+            services.AddSingleton<IServiceDiscovery, ServiceDiscovery>();
 
-            // add token providers so we can get them from DI in case they need other dependencies
+            // add token providers so it can get depedencies from DI, too
             services.AddScoped<UserAccessTokenProvider>();
             services.AddScoped<ClientCredentialsTokenProvider>();
 
@@ -60,9 +54,9 @@ namespace Runner
             {
                 // create a named/configured HttpClient
                 var httpClient = p.GetRequiredService<IHttpClientFactory>()
-                    .CreateClient(typeof(IPetStoreClient).Name);
+                    .CreateClient(nameof(IPetStoreClient));
 
-                // in this case we use user initiated flow
+                // in this case user initiated flow is used
                 var tokenProvider = p.GetRequiredService<UserAccessTokenProvider>();
 
                 // get the base Uri from service disco (service name could come from configuration again...)
@@ -70,6 +64,11 @@ namespace Runner
 
                 return new PetStoreClient(baseUri, httpClient, new TokenCredentials(tokenProvider));
             });
+
+            var provider = services.BuildServiceProvider();
+
+            // will throw as we are not in ASP.NET and no IHttpContextAccessor has been injected
+            var client = provider.GetRequiredService<IPetStoreClient>();
         }
     }
 
@@ -77,14 +76,14 @@ namespace Runner
     {
         private readonly IHttpContextAccessor _contextAccessor;
 
-        // http context can be used to retrieve the user's access token from claims, for user initiated flow
         public UserAccessTokenProvider(IHttpContextAccessor contextAccessor)
         {
-            contextAccessor = contextAccessor ?? throw new ArgumentNullException(nameof(contextAccessor));
+            _contextAccessor = contextAccessor ?? throw new ArgumentNullException(nameof(contextAccessor));
         }
 
         public async Task<AuthenticationHeaderValue> GetAuthenticationHeaderAsync(CancellationToken cancellationToken)
         {
+            // getting the token from current http context. Doesn't work if no user is signed in of course
             var token = await _contextAccessor.HttpContext?.GetTokenAsync("acccess_token");
             if (string.IsNullOrWhiteSpace(token))
             {
@@ -108,5 +107,13 @@ namespace Runner
     public interface IServiceDiscovery
     {
         Uri GetServiceBaseUri(string name);
+    }
+
+    public class ServiceDiscovery : IServiceDiscovery
+    {
+        public Uri GetServiceBaseUri(string name)
+        {
+            return new Uri("http://petstore.swagger.io/v2");
+        }
     }
 }
