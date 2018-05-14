f8f2de67f7107038186bc835d6f47530924af5d4
MichaC <kletterer@web.de>
2018-05-14 22:30:15 +0200

    more real example code with another, more simple constrcutor actually

--- src/Runner/Program.cs
+++ src/Runner/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Rest;
 using Polly;
@@ -22,12 +28,14 @@ namespace Runner
             })
             .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(4, (t) => TimeSpan.FromSeconds(t)));
 
-            services.Configure<ClientSettings>(nameof(PetStoreClient), settings =>
+            services.Configure<ClientSettings>(nameof(IPetStoreClient), settings =>
             {
                 // probably resolve base uri from service discovery...
+                // doesn't work well inside the configure factory because we don't have access to IServiceProvider...
                 settings.BaseUri = new Uri("http://petstore.swagger.io/v2");
 
                 // probably use ITokenProvider to get to user's token or retrieve a client credentials token...
+                // same here, how do we resolve a ITokenProvider for this client?
                 settings.Credentials = new TokenCredentials("superToken");
             });
 
@@ -38,6 +46,67 @@ namespace Runner
             var pets = client.FindPetsByStatus(new List<string>() { "available", "pending" });
 
             Console.WriteLine($"There are {pets.Count} pets.");
+
+            /*** more real implementation: ***/
+
+            // no implementation here, just dummy here...
+            services.AddSingleton<IServiceDiscovery>();
+
+            // add token providers so we can get them from DI in case they need other dependencies
+            services.AddScoped<UserAccessTokenProvider>();
+            services.AddScoped<ClientCredentialsTokenProvider>();
+
+            services.AddScoped<IPetStoreClient, PetStoreClient>(p =>
+            {
+                // create a named/configured HttpClient
+                var httpClient = p.GetRequiredService<IHttpClientFactory>()
+                    .CreateClient(typeof(IPetStoreClient).Name);
+
+                // in this case we use user initiated flow
+                var tokenProvider = p.GetRequiredService<UserAccessTokenProvider>();
+
+                // get the base Uri from service disco (service name could come from configuration again...)
+                var baseUri = p.GetRequiredService<IServiceDiscovery>().GetServiceBaseUri("petStore");
+
+                return new PetStoreClient(baseUri, httpClient, new TokenCredentials(tokenProvider));
+            });
         }
+    }
+
+    public class UserAccessTokenProvider : ITokenProvider
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        // http context can be used to retrieve the user's access token from claims, for user initiated flow
+        public UserAccessTokenProvider(IHttpContextAccessor contextAccessor)
+        {
+            contextAccessor = contextAccessor ?? throw new ArgumentNullException(nameof(contextAccessor));
+        }
+
+        public async Task<AuthenticationHeaderValue> GetAuthenticationHeaderAsync(CancellationToken cancellationToken)
+        {
+            var token = await _contextAccessor.HttpContext?.GetTokenAsync("acccess_token");
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new InvalidOperationException("Could not get an access token from HttpContext.");
+            }
+
+            return new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+
+    public class ClientCredentialsTokenProvider : ITokenProvider
+    {
+        public Task<AuthenticationHeaderValue> GetAuthenticationHeaderAsync(CancellationToken cancellationToken)
+        {
+            // do things to get a token from your authority for client secrets...
+            throw new NotImplementedException();
+        }
+    }
+
+    // dummy code here
+    public interface IServiceDiscovery
+    {
+        Uri GetServiceBaseUri(string name);
     }
 }
--- src/Shared/Clients/PetStoreClient.cs
+++ src/Shared/Clients/PetStoreClient.cs
@@ -1,31 +1,40 @@
 ﻿using System;
 using System.Net.Http;
 using Microsoft.Extensions.Options;
+using Microsoft.Rest;
 using Shared;
 
 namespace Swagger
 {
     public partial class PetStoreClient
     {
-        public PetStoreClient(IOptionsSnapshot<ClientSettings> options, HttpClient httpClient) : base(httpClient, false)
+        public PetStoreClient(IOptionsSnapshot<ClientSettings> options, HttpClient httpClient)
+            : base(httpClient, false)
         {
             if (options == null)
             {
                 throw new ArgumentNullException(nameof(options));
             }
 
-            var settings = options.Get(nameof(PetStoreClient));
+            var settings = options.Get(nameof(IPetStoreClient));
             if (settings?.BaseUri == null)
             {
-                throw new ArgumentNullException("baseUri");
+                throw new ArgumentNullException(nameof(settings.BaseUri));
             }
             if (settings?.Credentials == null)
             {
-                throw new System.ArgumentNullException("credentials");
+                throw new ArgumentNullException(nameof(settings.Credentials));
             }
 
             BaseUri = settings.BaseUri;
             Credentials = settings.Credentials;
+        }
+
+        public PetStoreClient(Uri baseUri, HttpClient httpClient, ServiceClientCredentials credentials)
+            : base(httpClient, false)
+        {
+            BaseUri = baseUri ?? throw new ArgumentNullException(nameof(baseUri));
+            Credentials = credentials ?? throw new ArgumentNullException(nameof(credentials));
         }
     }
 }
